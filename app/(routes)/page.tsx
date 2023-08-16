dedf51ef919a14fa6c07240db2bc513b53bee422
profabhishekjha <jhaabhishek910@gmail.com>
2023-08-16 12:19:03 +0530

    billboard-data-fetch

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,19 @@
+import getBillboard from "@/actions/get-billboard";
+import Container from "@/components/ui/container";
 import React from "react";
+import Billboard from "@/components/ui/billboard";
 
-const HomePage = () => {
-  return <div>Hello Store</div>;
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("3246bbcd-3486-4b3c-a8db-be71a8b8053c");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
