adc2d1ec504fc21843361b0b2341e859e03b98e8
profabhishekjha <jhaabhishek910@gmail.com>
2023-08-21 13:48:12 +0530

    checkout_page-final

--- components/preview-modal.tsx
+++ components/preview-modal.tsx
@@ -2,7 +2,8 @@
 
 import usePreviewModal from "@/hooks/use-preview-modals";
 import Modal from "@/components/ui/modal";
-import Info from "./info";
+import Info from "@/components/info";
+import Gallery from "@/components/gallery";
 
 const PreviewModal = () => {
   const previewModal = usePreviewModal();
@@ -15,8 +16,11 @@ const PreviewModal = () => {
   return (
     <Modal open={previewModal.isOpen} onClose={previewModal.onClose}>
       <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
-        <div className="sm:col-span-4 lg:col-span-7">
-          <Info data={product}></Info>
+        <div className="sm:col-span-4 lg:col-span-5">
+          <Gallery images={product.images} />
+        </div>
+        <div className="sm:col-span-0 lg:col-span-7">
+          <Info data={product} />
         </div>
       </div>
     </Modal>
